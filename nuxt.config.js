2f3f69c5fc70fc158575b466836b2841310f8e5e
katsup <brett_7y7@yahoo.ca>
2023-11-04 15:35:16 +0900

    update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ export default {
   },
    
   env: {
-      apiKey: 'AIzaSyBR4GaaaFuFgOsbdCeI4cTbuSubFpmHhcg',
+      apiKey: '',
     },
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
